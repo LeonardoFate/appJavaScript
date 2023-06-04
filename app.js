4331b29da6d879961bdd7f08a221da370205b770
LeonardoFate <dusteb14@gmail.com>
2023-06-04 03:37:47 -0500

    Creando tarjeta de agregar productos

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
 //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
-alert('Vas bien mijito')
+class Product{
+    constructor(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+
+    }
+
+
+}
+
+class UI{
+    addProduct(){
+        const productList =  document.getElementById('product-list');
+        const element = document.createElement('div');
+    }
+    deleteProduct(){
+
+    }
+
+    showMessage(){
+
+    }
+
+}
+
+//DOM Events
+
+document.getElementById('product-form')
+.addEventListener('submit' , function(e)  {
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
+    console.log(name, price, year);
+
+    const product = new product(name, price, year);
+
+
+e.preventDefault();
+});
